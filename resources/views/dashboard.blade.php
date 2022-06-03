cff3b7e6d9d1294305595281aed49ed7a7713caa
Andy Newhouse <hi@andymnewhouse.me>
2022-06-03 11:45:51 -0500

    Add time tile

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,7 @@
 <x-dashboard>
-    <livewire:calendar-tile position="a1" :calendar-id="config('dashboard.tiles.calendar.ids.tighten')" title="Tighten"/>
-    <livewire:calendar-tile position="b1" :calendar-id="config('dashboard.tiles.calendar.ids.institute')" title="Institute"/>
+    <livewire:date-time-tile position="a1" timezone="America/Chicago" title="Central Time" />
+    <livewire:calendar-tile position="a2" :calendar-id="config('dashboard.tiles.calendar.ids.tighten')" title="Tighten"/>
+    <livewire:calendar-tile position="a3" :calendar-id="config('dashboard.tiles.calendar.ids.institute')" title="Institute"/>
     <livewire:calendar-tile position="c1" :calendar-id="config('dashboard.tiles.calendar.ids.andy')" title="Andy"/>
     <livewire:calendar-tile position="d1" :calendar-id="config('dashboard.tiles.calendar.ids.jess')" title="Jess"/>
     <livewire:calendar-tile position="e1" :calendar-id="config('dashboard.tiles.calendar.ids.family')" title="Family"/>
---
+++ resources/views/vendor/dashboard-date-time-tile/tile.blade.php
@@ -0,0 +1,34 @@
+@php
+    $time = uniqid('time-');
+    $date = uniqid('date-');
+@endphp
+
+<x-dashboard-tile :position="$position">
+    <div class="grid gap-2 justify-items-center h-full text-center">
+        <div class="font-medium text-dimmed text-sm uppercase tracking-wide tabular-nums">
+            {{ $title ?? str_replace('_', ' ', $timezone) }}
+        </div>
+        <div id="{{ $time }}" class="self-center font-bold text-4xl tracking-wide leading-none">
+            {{ Carbon\Carbon::now($timezone)->isoFormat('LT') }}
+        </div>
+        <div id="{{ $date }}" class="text-sm uppercase text-dimmed">
+            {{ Carbon\Carbon::now($timezone)->isoFormat('LL') }}
+        </div>
+    </div>
+</x-dashboard-tile>
+
+@once
+    @push('scripts')
+        <script src="https://cdn.jsdelivr.net/npm/moment@2.x.x/min/moment-with-locales.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/moment-timezone@0.x.x/builds/moment-timezone-with-data.min.js"></script>
+    @endpush
+@endonce
+
+@push('scripts')
+    <script defer>
+        setInterval(() => {
+            document.getElementById('{{ $date }}').innerHTML = moment().locale('{{ config('app.locale') }}').tz('{{ $timezone }}').format('LL')
+            document.getElementById('{{ $time }}').innerHTML = moment().locale('{{ config('app.locale') }}').tz('{{ $timezone }}').format('LT')
+        }, 1000)
+    </script>
+@endpush
