4d841e8f37457f41804892d063930ad853e47466
Andy Newhouse <hi@andymnewhouse.me>
2022-09-01 22:27:35 -0500

    Update rendering of month, add polling, and refresh

--- app/Http/Livewire/Calendar.php
+++ app/Http/Livewire/Calendar.php
@@ -7,18 +7,30 @@ use Livewire\Component;
 class Calendar extends Component
 {
     public $position;
+    public $carbon;
+
+    public function mount()
+    {
+        $this->carbon = now();
+    }
 
     public function render()
     {
-        $time = time();
-        $numDay = date('d', $time);
-        $numMonth = date('m', $time);
-        $month = date('F', $time);
-        $year = date('Y', $time);
-        $firstDay = mktime(0, 0, 0, $numMonth, 1, $year);
-        $daysInMonth = cal_days_in_month(0, $numMonth, $year);
-        $dayOfWeek = date('w', $firstDay);
+        return view('livewire.calendar', [
+            'daysInMonth' => $this->carbon->daysInMonth,
+            'dayOfWeek' => $this->carbon->copy()->startOfMonth()->dayOfWeek,
+            'month' => $this->carbon->englishMonth,
+            'todaysDate' => $this->carbon->day,
+        ]);
+    }
 
-        return view('livewire.calendar', compact('numDay', 'numMonth', 'month', 'year', 'firstDay', 'daysInMonth', 'dayOfWeek'));
+    public function nextMonth()
+    {
+        $this->carbon = $this->carbon->addMonth();
+    }
+
+    public function prevMonth()
+    {
+        $this->carbon = $this->carbon->subMonth();
     }
 }
--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -1,37 +1,23 @@
-<x-dashboard-tile :position="$position">
-    <table class="table-auto w-full">
-        <caption class="text-lg mb-4">{{ $month }} {{ $year }}</caption>
-        <thead>
-            <tr>
-                <th abbr="Sunday" scope="col" title="Sunday" class="text-center text-dimmed">S</th>
-                <th abbr="Monday" scope="col" title="Monday" class="text-center text-dimmed">M</th>
-                <th abbr="Tuesday" scope="col" title="Tuesday" class="text-center text-dimmed">T</th>
-                <th abbr="Wednesday" scope="col" title="Wednesday" class="text-center text-dimmed">W</th>
-                <th abbr="Thursday" scope="col" title="Thursday" class="text-center text-dimmed">T</th>
-                <th abbr="Friday" scope="col" title="Friday" class="text-center text-dimmed">F</th>
-                <th abbr="Saturday" scope="col" title="Saturday" class="text-center text-dimmed">S</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-            @php
-            if(0 != $dayOfWeek) {
-                echo('<td colspan="'.$dayOfWeek.'" class="text-center"> </td>');
-            }
-            for($i=1; $i<=$daysInMonth; $i++) {
-                if($i == $numDay) {
-                    echo('<td id="today" class="text-center bg-accent rounded-md text-invers">');
-                } else {
-                    echo("<td class='text-center'>");
-                }
-                echo($i);
-                echo("</td>");
-                if(date('w', mktime(0,0,0,$numMonth, $i, $year)) == 6) {
-                    echo("</tr><tr>");
-                }
-            }
-            @endphp
-            </tr>
-        </tbody>
-    </table>
+<x-dashboard-tile :position="$position" refresh-interval="21600">
+    <div class="grid grid-cols-7 gap-1 w-full">
+        <button wire:click="prevMonth" class="text-lg text-center">&laquo;</button>
+        <div class="text-lg col-span-5 text-center">{{ $month }}</div>
+        <button wire:click="nextMonth" class="text-lg text-center">&raquo;</button>
+
+        <div abbr="Sunday" scope="col" title="Sunday" class="text-center text-dimmed">S</div>
+        <div abbr="Monday" scope="col" title="Monday" class="text-center text-dimmed">M</div>
+        <div abbr="Tuesday" scope="col" title="Tuesday" class="text-center text-dimmed">T</div>
+        <div abbr="Wednesday" scope="col" title="Wednesday" class="text-center text-dimmed">W</div>
+        <div abbr="Thursday" scope="col" title="Thursday" class="text-center text-dimmed">T</div>
+        <div abbr="Friday" scope="col" title="Friday" class="text-center text-dimmed">F</div>
+        <div abbr="Saturday" scope="col" title="Saturday" class="text-center text-dimmed">S</div>
+
+        @if ($dayOfWeek !== 0)
+        <div class="col-span-{{ $dayOfWeek }}"></div>
+        @endif
+
+        @foreach(range(1, $daysInMonth) as $date)
+        <div class="text-center {{ $date === $todaysDate ? 'bg-accent rounded-md text-invers' : ''}}">{{ $date }}</div>
+        @endforeach
+    </div>
 </x-dashboard-tile>
